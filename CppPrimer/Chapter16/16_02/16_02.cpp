dcf46957e7371104815f95e97ce8432a212bd32b
webary <813577991@qq.com>
2015-11-15 00:00:42 +0800

    Update 16_02.cpp

--- CppPrimer/Chapter16/16_02/16_02.cpp
+++ CppPrimer/Chapter16/16_02/16_02.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
+using namespace std;
 
 //比较两个输入参数的大小，第一个大于第二个返回1，小于则返回-1，相等则返回0
 template<typename T>
-int compare(const T &lhs, const T &rhs) {
-    if(lhs < rhs) {
+int compare(const T &lhs, const T &rhs)
+{
+    if(lhs < rhs)
         return -1;
-    }
-    if(rhs < lhs) {
+    if(rhs < lhs)
         return 1;
-    }
     return 0;
 }
 
 int main( ) {
-    int a = 1;
-    int b = 2;
-    std::cout << compare(1, 2) << std::endl;
+    int a = 1, b = 2;
+    cout << compare(1, 2) << endl;
     return 0;
 }
